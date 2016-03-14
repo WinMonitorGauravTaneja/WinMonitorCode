7aaa550c24faf0379623fe89dfaf0103895bf7d3
WinMonitorGauravTaneja <gaurav.taneja@winshuttle.com>
2016-03-14 18:11:26 +0530

    Updated App Before Entity Start

---
+++ WinMonitorApp/WinMonitorApp/Models/Company.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WinMonitorApp.Models
+{
+    public class Company
+    {
+        public String mstrCompId;
+        public String mstrCompName;
+        public String mstrCompUrl;
+
+    }
+}
